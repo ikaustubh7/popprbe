c7018c8c8cb9cf661766e4dc6f403b70a5f50bc5
Kaustubh <ikaustubh77@gmail.com>
2024-03-28 22:20:46 +0530

    done menu animation

--- app/components/NavbarMenuicon.jsx
+++ app/components/NavbarMenuicon.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import styles from './navbarmenuicon.module.css'
 import Menuicon from "../assets/Menuicon";
 import { useState } from "react";
+import path from 'path';
 
 
 
@@ -14,15 +15,51 @@ function NavbarMenuicon() {
   return (
 
     <div className='absolute  flex-col place-items-center   bg-[#201c1c]'>
-     <div className={`rounded-full place-items-center  mt-2  pt-2  ${styles.Menuicon}`}  onMouseEnter={()=>setIsHover(true)} onMouseLeave={()=>setIsHover(false)}    
+     <div className={`rounded-full place-items-center   -z-1   pt-3  ${styles.Menuicon}`}  onMouseEnter={()=>setIsHover(true)} onMouseLeave={()=>setIsHover(false)}    
      
      
-     
+
      >
      
-      <div className=' h-[54px] w-[54px] bg-[#a374ff] rounded-full relative    '  >
+      <div className=' h-[54px] w-[54px] bg-[#a374ff] rounded-full relative overflow-hidden  ' 
+        
+      
+      >
         
            <span className=' z-10 absolute top-[50%] left-[50%] -translate-y-[50%] -translate-x-[50%] '  ><Menuicon /></span> 
+
+           <div className={styles.bglayercontainer}
+  
+
+  >
+  <span className={`${styles.bglayer1}`} 
+  style={isHover ? {transform: 'translateY(-54px)' ,  transition: 'transform 1.3s cubic-bezier(.19,1,.22,1), opacity .3s linear'  } : {}} 
+  
+  
+  ></span>
+  
+  <span className={`${styles.bglayer2}`} 
+  style={isHover ? {transform: 'translateY(-54px)' ,  transition: 'transform 1.3s cubic-bezier(.19,1,.22,1), opacity .3s linear', transitionDelay:'0.1s' } : {}} 
+  
+  ></span>
+  
+  <span className={`${styles.bglayer3}`} 
+  style={isHover ? {transform: 'translateY(-54px)' ,  transition: 'transform 1.3s cubic-bezier(.19,1,.22,1), opacity .3s linear' , transitionDelay:'0.2s'  } : {}} 
+  
+  
+  ></span>
+  
+  
+  
+  </div>
+
+
+
+
+
+
+
+
             </div>
       
 
@@ -30,33 +67,9 @@ function NavbarMenuicon() {
 
 </div>
 
-<div className={styles.bglayercontainer}
 
 
 
->
-<span className={`${styles.bglayer1}`} 
-style={isHover ? {transform: 'translateY(-54px)' ,  transition: 'transform 1.3s cubic-bezier(.19,1,.22,1), opacity .3s linear'  } : {}} 
-
-
-></span>
-
-<span className={`${styles.bglayer2}`} 
-style={isHover ? {transform: 'translateY(-54px)' ,  transition: 'transform 1.3s cubic-bezier(.19,1,.22,1), opacity .3s linear', transitionDelay:'0.1s' } : {}} 
-
-></span>
-
-<span className={`${styles.bglayer3}`} 
-style={isHover ? {transform: 'translateY(-54px)' ,  transition: 'transform 1.3s cubic-bezier(.19,1,.22,1), opacity .3s linear' , transitionDelay:'0.2s'  } : {}} 
-
-
-></span>
-
-
-
-</div>
-
-<div  className={styles.bgmask}>  </div>
 
 
 
